b1408a1ec660436f02ff0db6a36b3c152bdeaf87
regionalace6-jpg <regionalace6@gmail.com>
2026-02-25 19:00:05 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const noblox = require('noblox.js');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+const prefix = "!";
+
+client.once("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === "roblox") {
+        const userId = interaction.options.getString("id");
+
+        try {
+            const user = await noblox.getUserInfo(Number(userId));
+
+            await interaction.reply({
+                content: `👤 Username: **${user.username}**
+🆔 User ID: **${user.id}**
+📅 Join Date: **${user.joinDate}**
+📦 Description: ${user.blurb || "No description"}`,
+            });
+
+        } catch (err) {
+            await interaction.reply("❌ Invalid Roblox ID.");
+        }
+    }
+});
+
+client.login("MTQ3NjIwODY2NDA5NTIzMjE1MQ.GzzUDF.1bpFFskGTRosZn_3iuD5OuBQo7EQeEkLKzg3bA");
